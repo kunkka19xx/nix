54bf940f4fa81561452758277c8c953b3393cdee
nobita079x@gmail.com <kunkka19xx@gmail.com>
2025-08-03 22:00:58 +0900

    Squashed 'dotfiles/nvim/' changes from adbb37a..2227bea
    2227bea it has issues, remove
    f3425bc a minimal theme
    8a2d8de sync
    c42b9fe rm unused plugins
    6f5f3d6 new desk
    1e83ae2 Merge commit '3869a7763410bdaba3cd3abf29681ef4228521cd'
    50f6217 hmm, for fun
    5aa18fd nicer resolution
    43cbff2 Merge commit 'b2415ebdabbc68c986bdee6e1348e5486bf0bd87'
    b862ccd update configs, add gcloud
    8e987bf merge
    3a8ef85 fix firefox in nixos
    3aae399 simple
    4a7ca8e Merge commit 'fd87dbae467b8fecf932fd08a4919d6c4ff952c9'
    25019d0 finish my setup, that's it
    d3b4ac2 this is my vm
    7bac42a java 17->23
    
    git-subtree-dir: dotfiles/nvim
    git-subtree-split: 2227bea5f4917ac8287ce0bcd0b2876c27197e0f

--- lua/vim-helpers.lua
+++ lua/vim-helpers.lua
@@ -36,11 +36,12 @@ vim.keymap.set("n", "<leader>ob", function()
 				cmd = "open -a 'Firefox' " .. file_path
 			end
 		else
-			cmd = "Firefox " .. file_path
-			local firefox_installed = vim.fn.system("which firefox > /dev/null 2>&1")
-			if firefox_installed ~= 0 then
-				cmd = "google-chrome " .. file_path
-			end
+            local firefox_path = vim.fn.system("which firefox"):gsub("\n", "")
+            local has_firefox = firefox_path ~= ""
+            if has_firefox then
+                cmd = "google-chrome " .. file_path
+            end
+			cmd = "firefox " .. file_path
 		end
 		os.execute(cmd .. " &")
 	else
@@ -51,51 +52,54 @@ end, { desc = "Open current file in browser" })
 -- set language based on vim mode
 -- requires im-select https://github.com/daipeihust/im-select
 -- recommend installing it by brew
-local function get_current_layout()
-	local f = io.popen("im-select")
-	local layout = nil
-	if f ~= nil then
-		layout = f:read("*all"):gsub("\n", "")
-		f:close()
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+if is_mac then
+	local function get_current_layout()
+		local f = io.popen("im-select")
+		local layout = nil
+		if f ~= nil then
+			layout = f:read("*all"):gsub("\n", "")
+			f:close()
+		end
+		return layout
 	end
-	return layout
+
+	-- Save current layout
+	local last_insert_layout = get_current_layout()
+	local english_layout = "com.apple.keylayout.ABC"
+
+	-- If exit insert mode, in command mode -> eng layout,
+	-- save the current layout to the variable, then use it for the
+	-- next insert time
+	vim.api.nvim_create_autocmd("InsertLeave", {
+		callback = function()
+			local current = get_current_layout()
+			last_insert_layout = current
+			os.execute("im-select " .. english_layout)
+		end,
+	})
+
+	-- mode change to normal -> eng layout
+	vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+		pattern = "*:*n",
+		callback = function()
+			os.execute("im-select " .. english_layout)
+		end,
+	})
+
+	-- when back to nvim, restore prev layout
+	vim.api.nvim_create_autocmd("InsertEnter", {
+		callback = function()
+			os.execute("im-select " .. last_insert_layout)
+		end,
+	})
+
+	vim.api.nvim_create_autocmd({ "FocusGained" }, {
+		callback = function()
+			os.execute("im-select " .. last_insert_layout)
+		end,
+	})
 end
-
--- Save current layout
-local last_insert_layout = get_current_layout()
-local english_layout = "com.apple.keylayout.ABC"
-
--- If exit insert mode, in command mode -> eng layout,
--- save the current layout to the variable, then use it for the
--- next insert time
-vim.api.nvim_create_autocmd("InsertLeave", {
-	callback = function()
-		local current = get_current_layout()
-		last_insert_layout = current
-		os.execute("im-select " .. english_layout)
-	end,
-})
-
--- mode change to normal -> eng layout
-vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
-	pattern = "*:*n",
-	callback = function()
-		os.execute("im-select " .. english_layout)
-	end,
-})
-
--- when back to nvim, restore prev layout
-vim.api.nvim_create_autocmd("InsertEnter", {
-	callback = function()
-		os.execute("im-select " .. last_insert_layout)
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "FocusGained" }, {
-	callback = function()
-		os.execute("im-select " .. last_insert_layout)
-	end,
-})
 
 vim.api.nvim_create_user_command("ShowTree", function()
 	local buf = vim.api.nvim_create_buf(false, true)
