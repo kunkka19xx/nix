5cd31524c6fb9285b287296c2f136805ea77afbf
kunkka19xx <nobita079x@gmail.com>
2025-08-18 16:45:27 +0900

    remove catppuccin

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -4,7 +4,6 @@
   programs.tmux = {
     plugins = [
       pkgs.tmuxPlugins.vim-tmux-navigator
-      pkgs.tmuxPlugins.catppuccin
     ];
     enable = true;
     extraConfig = builtins.readFile ../../dotfiles/tmux/tmux.conf;
